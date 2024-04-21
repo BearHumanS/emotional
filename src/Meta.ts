6416f9452f49d50d0b033d3208131c93396c3b45
BearHumanS <bye0651@naver.com>
2024-04-22 03:31:26 +0900

    Feat: 메타 태그 데이터 추가

---
+++ src/Meta.ts
@@ -0,0 +1,22 @@
+export const Meta = {
+  title: 'emotional',
+  siteName: 'emotional',
+  description:
+    '자신의 감정을 표현하면 그에 맞는 추천 노래 Playlist를 알려드려요.',
+  keywords: [
+    '일기',
+    '감정',
+    '노래',
+    '음악',
+    '추천',
+    'playlist',
+    'youtube',
+    '알고리즘',
+    'ai',
+    'gpt',
+    '유튜브',
+  ],
+  url: 'https://www.emotional.today/',
+  naverVerification: 'cde8956ade8797ebd60205e32864a43ffad92e24',
+  image: '/og-imgae.jpg',
+};
