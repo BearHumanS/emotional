b58a0ca3a0302ca88251970d56be88a56fa5b1cf
BearHumanS <bye0651@naver.com>
2024-04-21 16:49:39 +0900

    Chore: viewport 관련 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
+import { Viewport } from 'next/types';
 import { Inter } from 'next/font/google';
 import './globals.css';
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 
 const inter = Inter({
   subsets: ['latin'],
