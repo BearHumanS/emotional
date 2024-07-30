c32b9fc087aeca1a92a73a0d3f7c8f4dd8df475b
BearHumanS <bye0651@naver.com>
2024-07-30 18:23:36 +0900

    chore: 빌드 스크립트 수정

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
+
+# 상위 디렉터리로 이동
 cd ../
-mkdir output
+
+# output 디렉터리 생성
+mkdir -p output
+
+# emotional 디렉터리의 모든 파일을 output 디렉터리로 복사
 cp -R ./emotional/* ./output
+
+# output 디렉터리를 emotional 디렉터리로 복사
 cp -R ./output ./emotional/
+
+# 권한 문제를 피하기 위해 필요한 권한 설정
+chmod -R 775 ./output
+chmod -R 775 ./emotional/output
