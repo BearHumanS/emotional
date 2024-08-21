4035810c05962a7b56c1d5b4374d6ec8526a37c2
BearHumanS <bye0651@naver.com>
2024-08-21 17:32:55 +0900

    feat: 공통으로 사용하는 fetchData 적용 및 엔드포인트 변경

--- src/app/api/analyze/analyze.ts
+++ src/app/api/analyze/analyze.ts
@@ -1,11 +1,9 @@
-import axios from 'axios';
+import { fetchData } from '../fetchData';
 
 export const analyzeDiary = async (prompt: string) => {
   try {
-    const res = await axios.post(
-      `/api/analyze?prompt=${encodeURIComponent(prompt)}`,
-    );
-    return res.data.keyword;
+    const res = await fetchData('/analyzeDiary', 'post', { prompt: prompt });
+    return res.keyword;
   } catch (error) {
     console.error(error);
     throw new Error('프롬프트 분석 실패');
