cb5fdbdfcacf04606382c9d17351d3b72f46a8bc
BearHumanS <bye0651@naver.com>
2024-08-24 00:22:37 +0900

    chore: 불필요한 파일 삭제 및 스크립트 변경

--- src/features/main/api/getYoutubeList.ts
+++
@@ -1,8 +0,0 @@
-import { youtube_v3 } from 'googleapis';
-
-export type GetYoutubeListRequestParams = Pick<
-  youtube_v3.Params$Resource$Search$List,
-  'maxResults' | 'q'
->;
-
-export type GetYoutubeListResponse = {};
