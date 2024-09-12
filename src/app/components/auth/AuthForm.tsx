5f7c99bebf77f567f5e7f821e977b535297e2aea
BearHumanS <bye0651@naver.com>
2024-09-12 23:08:42 +0900

    feat: 폼 전환 시 데이터 상태 관리

--- src/app/components/auth/AuthForm.tsx
+++ src/app/components/auth/AuthForm.tsx
@@ -1,18 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import RegisterForm from '../register/RegisterForm';
 import LoginForm from '../loginForm/LoginForm';
 
+type FormRef = {
+  resetForm: () => void;
+};
+
 const AuthForm = () => {
   const [isLoginActive, setIsLoginActive] = useState(true);
-  const [loading, setLoading] = useState(true); // 해시값 확인 전 로딩 상태 추가
-  const [isSwitching, setIsSwitching] = useState(false); // 전환 중인지 확인
+  const [loading, setLoading] = useState(true);
+  const [isSwitching, setIsSwitching] = useState(false);
 
-  // URL 해시값에 따라 폼 전환
+  const loginFormRef = useRef<FormRef>(null);
+  const registerFormRef = useRef<FormRef>(null);
+
   useEffect(() => {
     const hash = window.location.hash;
 
     if (!hash) {
-      // URL에 해시가 없으면 기본값으로 #signin 설정
       window.location.hash = '#signin';
       setIsLoginActive(true);
     } else if (hash === '#signup') {
@@ -24,22 +29,27 @@ const AuthForm = () => {
     setLoading(false);
   }, []);
 
-  // 폼 전환 시 URL 해시값 변경 및 일시적 클릭 차단
   const handleFormSwitch = (formType: 'signin' | 'signup') => {
-    if (isSwitching) return; // 전환 중이면 클릭 차단
+    if (isSwitching) return;
 
-    setIsSwitching(true); // 전환 중 상태로 설정
+    setIsSwitching(true);
 
     setTimeout(() => {
       if (formType === 'signin') {
         setIsLoginActive(true);
         window.location.hash = '#signin';
+        if (registerFormRef.current) {
+          registerFormRef.current.resetForm(); // 회원가입 폼 초기화
+        }
       } else {
         setIsLoginActive(false);
         window.location.hash = '#signup';
+        if (loginFormRef.current) {
+          loginFormRef.current.resetForm(); // 로그인 폼 초기화
+        }
       }
-      setIsSwitching(false); // 전환 후 다시 클릭 허용
-    }, 500); // 0.5초 후 전환 완료
+      setIsSwitching(false);
+    }, 500);
   };
 
   if (loading) {
@@ -47,17 +57,15 @@ const AuthForm = () => {
   }
 
   return (
-    //s
     <section className="h-full flex justify-center items-center min-h-screen">
       <div
         className={`relative h-full w-[460px] ${isLoginActive ? 'bg-indigo-600' : 'bg-purple-500'}  rounded-lg p-8 overflow-hidden shadow-md flex justify-center items-center transition-colors duration-500`}
       >
-        {/* 로그인 폼 */}
         <article className={`${isLoginActive ? 'block' : 'hidden'} mb-24`}>
           <div>
             <h2 className="sr-only">로그인</h2>
           </div>
-          <LoginForm />
+          <LoginForm ref={loginFormRef} />
           <div className="text-center mt-4 text-white">
             계정이 없으신가요?{' '}
             <button
@@ -69,12 +77,11 @@ const AuthForm = () => {
           </div>
         </article>
 
-        {/* 회원가입 폼 */}
         <article className={`${isLoginActive ? 'hidden' : 'block'} mb-24`}>
           <div>
             <h2 className="sr-only">회원가입</h2>
           </div>
-          <RegisterForm />
+          <RegisterForm ref={registerFormRef} />
           <div className="text-center mt-4 text-white">
             이미 계정이 있으신가요?{' '}
             <button
