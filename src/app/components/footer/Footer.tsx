19bdeb9f9657aa60aad47e0231c70a66545394aa
BearHumanS <bye0651@naver.com>
2024-09-29 17:55:31 +0900

    style: footer 색상 변경

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="absolute bottom-2 text-slate-300">
+    <div className="absolute bottom-2 text-yellow-500">
       Copyright 2024. BearHumanS all rights reserved.
     </div>
   );
