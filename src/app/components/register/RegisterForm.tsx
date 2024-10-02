60d50e180ad30886c44371d4a7094c2aaeb85e21
BearHumanS <bye0651@naver.com>
2024-10-03 02:10:46 +0900

    refactor: 불필요한 로직 제거 및 이메일 인증 추가 및 비밀번호 재확인 기능 추가

--- src/app/components/register/RegisterForm.tsx
+++ src/app/components/register/RegisterForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { fetchRegister } from '@/app/api/userApi';
+import { fetchRegister, sendVerification } from '@/app/api/userApi';
 import { emailRegex, passwordRegex } from '@/lib/constants/constants';
 import useShowPasswordStore from '@/lib/store/useShowPasswordStore';
 import { useRouter } from 'next/navigation';
@@ -14,15 +14,23 @@ import {
 } from 'react';
 import { toast } from 'react-toastify';
 import ChangeIcons from '../ChangeIcons';
+import { useMutation } from '@tanstack/react-query';
+import UseAnimations from 'react-useanimations';
+import loading from 'react-useanimations/lib/loading';
+import { formatTime } from '@/lib/util/formatTime';
+import useShowConfirmPasswordStore from '@/lib/store/useShowConfirmPasswordStore';
 
 const RegisterForm = forwardRef((_, ref) => {
   const [userId, setUserId] = useState('');
   const [userPassword, setUserPassword] = useState('');
-  /*  const [errors, setErrors] = useState({ id: '', password: '' }); */
-
-  const [debouncedEmail, setDebouncedEmail] = useState(userId);
-  const [debouncedPassword, setDebouncedPassword] = useState(userPassword);
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [verificationCode, setVerificationCode] = useState('');
   const { showPassword, setShowPassword } = useShowPasswordStore();
+  const { showConfirmPassword, setShowConfirmPassword } =
+    useShowConfirmPasswordStore();
+  const [visiblity, setivsiblity] = useState(false);
+  const [timer, setTimer] = useState(300);
+  const [isExpired, setIsExpired] = useState(false);
 
   const router = useRouter();
 
@@ -30,72 +38,56 @@ const RegisterForm = forwardRef((_, ref) => {
     resetForm() {
       setUserId('');
       setUserPassword('');
+      setConfirmPassword('');
+      setVerificationCode('');
       setShowPassword(false);
-      /* setErrors({ id: '', password: '' }); */
+      setivsiblity(false);
+      setShowConfirmPassword(false);
+      setIsExpired(false);
     },
   }));
 
-  // 이메일과 비밀번호에 대한 디바운스 로직 적용
+  // 타이머
   useEffect(() => {
-    const emailHandler = setTimeout(() => {
-      setDebouncedEmail(userId);
-    }, 500);
+    let counter: string | number | NodeJS.Timeout | undefined;
 
-    return () => {
-      clearTimeout(emailHandler);
-    };
-  }, [userId]);
-
-  useEffect(() => {
-    const passwordHandler = setTimeout(() => {
-      setDebouncedPassword(userPassword);
-    }, 500);
-
-    return () => {
-      clearTimeout(passwordHandler);
-    };
-  }, [userPassword]);
-
-  useEffect(() => {
-    if (debouncedEmail !== '') {
-      validateEmail(debouncedEmail);
-    } else {
-      /* setErrors((prev) => ({ ...prev, id: '' })); */
+    if (visiblity && timer > 0) {
+      counter = setInterval(() => {
+        setTimer((prev) => prev - 1);
+      }, 1000);
+    } else if (timer === 0) {
+      setIsExpired(true);
+      clearInterval(counter);
     }
-  }, [debouncedEmail]);
 
-  useEffect(() => {
-    if (debouncedPassword !== '') {
-      validatePassword(debouncedPassword);
-    } else {
-      /* setErrors((prev) => ({ ...prev, password: '' })); */
-    }
-  }, [debouncedPassword]);
+    return () => clearInterval(counter);
+  }, [timer, visiblity]);
 
   const validateEmail = (email: string) => {
     if (!emailRegex.test(email)) {
-      /* setErrors((prev) => ({ ...prev, id: '이메일 형식에 맞지 않습니다.' })); */
       toast.warn('이메일 형식에 맞지 않습니다.');
       return false;
     } else {
-      /* setErrors((prev) => ({ ...prev, id: '' })); */
       return true;
     }
   };
 
   const validatePassword = (password: string) => {
     if (!passwordRegex.test(password)) {
-      /* setErrors((prev) => ({
-        ...prev,
-        password:
-          '비밀번호는 문자, 숫자, 특수 문자를 포함하여 8자 이상이어야 합니다.',
-      })); */
       toast.warn(
         '비밀번호는 문자, 숫자, 특수 문자를 포함하여 8자 이상이어야 합니다.',
       );
       return false;
     } else {
-      /* setErrors((prev) => ({ ...prev, password: '' })); */
+      return true;
+    }
+  };
+
+  const validateConfirmPassword = (password: string) => {
+    if (password !== userPassword) {
+      toast.warn('비밀번호가 일치하지 않습니다.');
+      return false;
+    } else {
       return true;
     }
   };
@@ -108,47 +100,79 @@ const RegisterForm = forwardRef((_, ref) => {
     setUserPassword(e.target.value);
   };
 
+  const onConfirmPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const onVerificationCodeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setVerificationCode(e.target.value);
+  };
+
+  const mutation = useMutation({
+    mutationFn: sendVerification,
+    onSuccess() {
+      if (isExpired) {
+        toast.warn('인증 코드가 만료되었습니다. 다시 요청해주세요.');
+        return;
+      }
+      toast.success('인증 코드를 발송했습니다');
+      setivsiblity(true);
+    },
+    onError() {
+      toast.error('인증 코드 발송에 실패했습니다.');
+      setivsiblity(false);
+    },
+  });
+
+  const onSendVerifcation = () => {
+    mutation.mutate(userId);
+  };
+
   const onRegisterSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
-    const isEmailValid = validateEmail(debouncedEmail);
-    const isPasswordValid = validatePassword(debouncedPassword);
+    const isEmailValid = validateEmail(userId);
+    const isPasswordValid = validatePassword(userPassword);
+    const isConfirmPasswordValid = validateConfirmPassword(confirmPassword);
 
-    if (!isEmailValid || !isPasswordValid) {
+    if (!isEmailValid || !isPasswordValid || !isConfirmPasswordValid) {
       return;
     }
 
-    if (debouncedEmail && debouncedPassword) {
+    if (userPassword !== confirmPassword) {
+      toast.warn('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+
+    if (userId && userPassword && verificationCode) {
       try {
         await fetchRegister({
-          email: debouncedEmail,
-          password: debouncedPassword,
+          email: userId,
+          password: userPassword,
+          confirmPassword,
+          verificationCode,
         });
 
-        toast.success('회원가입을 환경합니다! 로그인해주세요!');
+        toast.success('회원가입을 환영합니다! 로그인해주세요!');
         router.push('/');
       } catch (error) {
-        console.error('회원가입 실패:', error);
-        toast.error('회원가입에 실패했습니다. 다시 시도해주세요.');
+        if (error instanceof Error) {
+          console.error('회원가입 실패:', error.message);
+          toast.error(error.message);
+        } else {
+          console.error('회원가입 실패: 알 수 없는 에러', error);
+          toast.error('알 수 없는 오류가 발생했습니다.');
+        }
       }
     }
   };
 
-  useEffect(() => {
-    const inputs = document.querySelectorAll('input');
-
-    inputs.forEach((input) => {
-      if (input.value !== '') {
-        const label = input.nextElementSibling;
-        if (label) {
-          label.classList.add('peer-valid');
-        }
-      }
-    });
-  }, []);
-
   const togglePasswordVisibility = () => {
     setShowPassword((prev) => !prev);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword((prev) => !prev);
   };
 
   return (
@@ -160,33 +184,77 @@ const RegisterForm = forwardRef((_, ref) => {
         onSubmit={onRegisterSubmit}
         className="flex flex-col gap-4 w-[305px]"
       >
-        <div className="relative group">
-          <input
-            type="text"
-            id="register-id"
-            placeholder=" "
-            value={userId}
-            onChange={onIdChange}
-            required
-            className="w-full p-2 border border-gray-300 rounded-lg bg-gray-100 outline-none peer mt-2"
-          />
-          <label
-            htmlFor="register-id"
-            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 transition-all duration-300 ease-in-out peer-placeholder-shown:top-[60%] peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-[-2px] peer-focus:text-sm peer-focus:text-yellow-500 peer-valid:top-[-2px] peer-valid:text-sm peer-valid:text-white peer-focus:left-1 peer-valid:left-1"
-          >
-            ID
-          </label>
-          {/*  {errors.id && (
-            <span className="text-yellow-500 font-bold">{errors.id}</span>
-          )} */}
+        <div className="flex justify-between items-center">
+          <div className="relative">
+            <input
+              type="text"
+              id="register-id"
+              placeholder=" "
+              value={userId}
+              onChange={onIdChange}
+              onBlur={() => validateEmail(userId)}
+              required
+              className="w-full p-2 border border-gray-300 rounded-lg bg-gray-100 outline-none peer mt-2"
+            />
+            <label
+              htmlFor="register-id"
+              className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 transition-all duration-300 ease-in-out peer-placeholder-shown:top-[60%] peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-[-2px] peer-focus:text-sm peer-focus:text-yellow-500 peer-valid:top-[-2px] peer-valid:text-sm peer-valid:text-white peer-focus:left-1 peer-valid:left-1"
+            >
+              ID
+            </label>
+          </div>
+          <div className="h-[50px] flex items-end">
+            <button
+              type="button"
+              disabled={mutation.isPending}
+              onClick={onSendVerifcation}
+              className={`h-[42px] w-[50px] bg-gray-200 outline-none rounded-lg ${
+                mutation.isPending ? 'cursor-not-allowed' : 'cursor-pointer'
+              } font-semibold hover:text-gray-200 hover:bg-black transtion-color duration-300`}
+            >
+              {mutation.isPending ? (
+                <div className="flex justify-center">
+                  <UseAnimations
+                    animation={loading}
+                    size={24}
+                    speed={2}
+                    strokeColor="gray"
+                  />
+                </div>
+              ) : (
+                '인증'
+              )}
+            </button>
+          </div>
         </div>
-        <div className="relative group">
+
+        {visiblity && (
+          <div className="relative flex justify-between items-center">
+            <input
+              type="text"
+              placeholder="인증코드"
+              value={verificationCode}
+              onChange={onVerificationCodeChange}
+              required
+              className="w-full p-2 border border-gray-300 rounded-lg bg-gray-100 outline-none peer mt-2"
+            />
+            <div className="flex w-[65%] justify-center gap-2">
+              <p className="text-yellow-500">남은 시간:</p>
+              <p className="w-5 text-gray-200">
+                {isExpired ? '만료됨' : `${formatTime(timer)}`}
+              </p>
+            </div>
+          </div>
+        )}
+
+        <div className="relative">
           <input
             type={showPassword ? 'text' : 'password'}
             id="register-password"
             placeholder=" "
             value={userPassword}
             onChange={onPasswordChange}
+            onBlur={() => validatePassword(userPassword)}
             required
             className="w-full p-2 border border-gray-300 rounded-lg bg-gray-100 outline-none peer mt-2"
           />
@@ -196,10 +264,30 @@ const RegisterForm = forwardRef((_, ref) => {
           >
             Password
           </label>
-          <ChangeIcons onClick={togglePasswordVisibility} />
-          {/*   {errors.password && (
-            <span className="text-yellow-500 font-bold">{errors.password}</span>
-          )} */}
+          <ChangeIcons onClick={togglePasswordVisibility} show={showPassword} />
+        </div>
+
+        <div className="relative">
+          <input
+            type={showConfirmPassword ? 'text' : 'password'}
+            id="confirm-password"
+            placeholder=""
+            value={confirmPassword}
+            onChange={onConfirmPasswordChange}
+            onBlur={() => validateConfirmPassword(confirmPassword)}
+            required
+            className="w-full p-2 border border-gray-300 rounded-lg bg-gray-100 outline-none peer mt-2"
+          />
+          <label
+            htmlFor="confirm-password"
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 transition-all duration-300 ease-in-out peer-placeholder-shown:top-[60%] peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-[-2px] peer-focus:text-sm peer-focus:text-yellow-500 peer-valid:top-[-2px] peer-valid:text-sm peer-valid:text-white peer-focus:left-1 peer-valid:left-1"
+          >
+            Confirm Password
+          </label>
+          <ChangeIcons
+            onClick={toggleConfirmPasswordVisibility}
+            show={showConfirmPassword}
+          />
         </div>
 
         <button
